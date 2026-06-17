63723b19b88ae7725fbd401b16acae65b529309f
Felipe Caceres - Fernandez <cfelipef824@gmail.com>
2026-06-17 00:59:25 -0700

    Hide project links from list, outline explore button, update project detail

--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -6,16 +6,6 @@ export default function ProjectListItem({ project, onClick }) {
         {project.description && <p className="desc">{project.description}</p>}
       </div>
       {project.detail && <p className="detail">{project.detail}</p>}
-      {project.links?.length > 0 && (
-        <p className="project-links" onClick={e => e.stopPropagation()}>
-          {project.links.map((link, i) => (
-            <span key={link.key + link.url}>
-              {i > 0 && <span className="sep">·</span>}
-              <a href={link.url}>{link.label ?? link.key}</a>
-            </span>
-          ))}
-        </p>
-      )}
     </li>
   );
 }
